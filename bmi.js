9b60ba4e28ba6b400a0f4de6000b78526479277c
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-03-16 21:44:33 +0530

    all changes done

---
+++ bmi.js
@@ -0,0 +1,104 @@
+const calculateButton = document.getElementById("calcBtn");
+const resetButton = document.getElementById("resetBtn");
+const weightInput = document.getElementById("weightInput");
+const heightInput = document.getElementById("heightInput");
+const resultBmiScore = document.getElementById("resultBmiScore");
+const resultBmiCategory = document.getElementById("resultBmiCategory");
+const bmiIndicator = document.getElementById("bmiIndicator");
+
+calculateButton.addEventListener("click", () => {
+    calculateBmiScore();
+
+});
+
+resetButton.addEventListener("click", () => {
+    reset();
+});
+
+function calculateBmiScore() {
+    const weight = parseFloat(weightInput.value);
+    const height = parseFloat(heightInput.value) / 100;
+
+    if(weight > 0 && height > 0) {
+        const bmiScore = (weight / (height ** 2)).toFixed(1);
+        resultBmiScore. textContent = bmiScore;
+        showResultBmiCategory(bmiScore);
+        showBmiIndicator(bmiScore);
+    }
+    else{
+        alert("Please enter a valid weight and height!")
+    }
+};
+
+function showResultBmiCategory(bmiScore) {
+    if (bmiScore < 18.5) {
+        resultBmiScore.style.color = "#0000ff";
+        resultBmiCategory.textContent = "Underweight";
+    }
+
+    else if (bmiScore >= 18.5 && bmiScore <= 24.9) {
+        resultBmiScore.style.color = "#008000";
+        resultBmiCategory.textContent = "Normal";
+    }
+
+    else if (bmiScore >= 25 && bmiScore <= 29.9) {
+        resultBmiScore.style.color = "#ffa500";
+        resultBmiCategory.textContent = "Overweight";
+    }
+
+    else {
+        resultBmiScore.style.color = "#ff0000";
+        resultBmiCategory.textContent = "Obese";
+    }
+}
+
+function showBmiIndicator(bmiScore) {
+    let firstScoreRange, lastScoreRange
+    let firstPercentRange, lastPercentRange
+
+    if (bmiScore < 18.5) {
+        firstScoreRange = 0
+        lastScoreRange = 18.49
+
+        firstPercentRange = 0
+        lastPercentRange = 25
+    }
+
+    else if (18.5 <= bmiScore <= 24.9) {
+        firstScoreRange = 18.5
+        lastScoreRange = 24.9
+
+        firstPercentRange = 25
+        lastPercentRange = 50
+    }
+
+    else if (25 <= bmiScore <= 29.9) {
+        firstScoreRange = 25
+        lastScoreRange = 39.9
+
+        firstPercentRange = 50
+        lastPercentRange = 75
+    }
+
+    else {
+        firstScoreRange = 30
+        lastScoreRange = 40
+
+        firstPercentRange = 75
+        lastPercentRange = 100
+    }
+
+    const slope = (lastPercentRange - firstPercentRange) / (lastScoreRange - firstScoreRange);
+    const intercept = firstPercentRange - slope * firstScoreRange
+    const percentage = Math.min(slope * bmiScore + intercept, 100);
+    bmiIndicator.style.left = percentage + "%"
+}
+
+function reset() {
+    weightInput.value = "";
+    heightInput.value = "";
+    resultBmiScore.textContent = "0";
+    resultBmiScore.style.color = "black";
+    resultBmiCategory.textContent = "N/A";
+    bmiIndicator.style.left = "0%";
+}
